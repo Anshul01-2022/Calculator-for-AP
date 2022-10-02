bda36cc29df9c9932dc4000faa5797022567cd86
Devtanu Misra <67796502+devtanumisra@users.noreply.github.com>
2022-10-02 12:09:27 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -2,3 +2,6 @@
 x = int(input("Please enter a number?"))
 y = int(input("Enter another number?"))
 print("Sum is" x+y)
+print("Subtraction is" x-y)
+print("Multiplication is" x*y)
+print("Division is" x/y)
